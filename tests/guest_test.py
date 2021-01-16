d90baae1e0aa581ff78a6c25c0778e14308f3dde
Jennifer W <jenniferw@Jennifers-MacBook-Pro.local>
2021-01-16 12:51:18 +0000

    test guest has name passed

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -2,4 +2,9 @@ import unittest
 from classes.guest import Guest
 
 class TestGuest(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.guest = Guest("Elton John")
+
+    
+    def test_guest_has_name(self):
+        self.assertEqual("Elton John", self.guest.name)
