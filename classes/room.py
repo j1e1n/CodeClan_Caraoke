0540513953938b3f2c641b1405d132ee1b8644be
Jennifer W <jenniferw@Jennifers-MacBook-Pro.local>
2021-01-16 12:47:42 +0000

    test song has name and artist passed

--- classes/room.py
+++ classes/room.py
@@ -1,5 +1,5 @@
 class Room:
-    def __init__(self, room_number)
-    self.room_number = room_number
-    self.song_list = []
-    self.guests = []
+    def __init__(self, room_number):
+        self.room_number = room_number
+        self.song_list = []
+        self.guests = []
--- tests/song_test.py
+++ tests/song_test.py
@@ -2,4 +2,16 @@ import unittest
 from classes.song import Song
 
 class TestSong(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.song = Song("Africa", "Toto")
+
+
+
+    def test_song_has_title(self):
+        self.assertEqual("Africa", self.song.title)
+
+    def test_song_has_artist(self):
+        self.assertEqual("Toto", self.song.artist)
+
+
+    
