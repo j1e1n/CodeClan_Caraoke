54a20f64a765c6573b0732bd2c0f035487382fc7
Jennifer W <jenniferw@Jennifers-MacBook-Pro.local>
2021-01-17 14:42:24 +0000

    test room can add song and guest passed

--- classes/room.py
+++ classes/room.py
@@ -3,3 +3,16 @@ class Room:
         self.room_name = room_name
         self.song_list = []
         self.guests = []
+
+    
+    def add_song(self, song):
+        self.song_list.append(song)
+
+    
+    def add_guest(self, guest):
+        self.guests.append(guest)
+        
+
+
+    
+    
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,9 +1,13 @@
 import unittest
 from classes.room import Room
+from classes.song import Song
+from classes.guest import Guest
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
         self.room = Room("Room 1")
+        self.song = Song("Africa", "Toto")
+        self.guest = Guest("Elton John")
 
     
     def test_room_has_name(self):
@@ -16,3 +20,13 @@ class TestRoom(unittest.TestCase):
     
     def test_room_starts_with_empty_guest_list(self):
         self.assertEqual(0, len(self.room.guests))
+
+
+    def test_can_add_song(self):
+        self.room.add_song(self.song)
+        self.assertEqual(1, len(self.room.song_list))
+
+    
+    def test_can_add_guest(self):
+        self.room.add_guest(self.guest)
+        self.assertEqual(1, len(self.room.guests))
